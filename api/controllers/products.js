c230cd98fda3018b0954e57341aee9dc36bb5496
bridgetrosefitz <bridgetrosefitz@gmail.com>
2025-02-03 18:21:08 +1100

    server filtering and commented scores logic

--- api/controllers/products.js
+++ api/controllers/products.js
@@ -1,23 +1,47 @@
 import axios from "axios";
 const jsonServerUrl = "http://localhost:4000"; // Adjust if necessary
 
+// const scoreContributions = {
+//   Humane: 1,
+//   "Locally Produced": 1,
+//   Healthy: 1,
+//   "Plastic-Free": 2,
+//   Unhealthy: -1,
+//   Wasteful: -1,
+// };
+
 const getProducts = async (req, res) => {
   try {
-    const response = await axios.get(`${jsonServerUrl}/products`);
-    let filteredProducts = [];
-    if (req.query.characteristic) {
-      filteredProducts = response.data.filter(product =>
-        product.characteristics.includes(req.query.characteristic)
-      );
-    } else {
-      filteredProducts = response.data;
-    }
-    res.json(filteredProducts);
+    const filteredResponse = await axios.get(
+      `${jsonServerUrl}/products?characteristics[0]=Humane`
+    );
+    // let filteredProducts = [];
+    // if (req.query.characteristic) {
+    //   filteredProducts = response.data.filter(product =>
+    //     product.characteristics.includes(req.query.characteristic)
+    //   );
+    // } else {
+    //   filteredProducts = response.data;
+    // }
+    res.send(filteredResponse.data);
   } catch (error) {
     console.error("Error fetching posts:", error);
     res.status(500).send("Error fetching posts");
   }
 };
+
+// const getScores = async (_req, res) => {
+//   const response = await axios.get(`${jsonServerUrl}/products`);
+//   const productScores = response.data.map(product => {
+//     const score = product.characteristic.reduce(
+//       (acc, curr) => (acc += scoreContributions[curr]),
+//       0
+//     );
+
+//     return { ...product, score };
+//   });
+//   res.json(productScores);
+// };
 
 // TO DO: Make this case insensitive
 // TO DO: Add error handling
@@ -25,4 +49,4 @@ const getProducts = async (req, res) => {
 // TO DO: error handling for if no results with given filter
 // TO DO: import filters and make filters dynamic
 
-export default { getProducts };
+export default getProducts;
--- api/routes/products.js
+++ api/routes/products.js
@@ -1,8 +1,8 @@
 import express from "express";
-import productsController from "../controllers/products.js";
+import getProducts from "../controllers/products.js";
 
 const router = express.Router();
 
-router.get("/", productsController.getProducts);
+router.get("/", getProducts);
 
 export default router;
