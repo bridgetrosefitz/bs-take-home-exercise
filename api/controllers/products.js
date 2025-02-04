a08c84fdb0e0ba81a2941b496caf7f24b4e275fb
bridgetrosefitz <bridgetrosefitz@gmail.com>
2025-02-04 22:41:02 +1100

    add score calc to filteredProducts controller method

--- api/controllers/products.js
+++ api/controllers/products.js
@@ -1,17 +1,23 @@
 import * as db from "../db/index.js";
 
+const totalScoreCalcQuery = `CAST(COALESCE(SUM(c.score_value), 0) AS INTEGER) AS total_score`;
+
 const getFilteredProducts = async (req, res) => {
   // TO DO: Error handling
-  const queryArgs =
-    req.query.characteristic === undefined
-      ? [`SELECT * FROM products`]
-      : [
-          `SELECT p.* FROM products p 
-    JOIN products_characteristics pc ON p.id = pc.product_id 
-    JOIN characteristics c ON pc.characteristic_id = c.id 
-    WHERE c.name = $1`,
-          [req.query.characteristic],
-        ];
+  const hasFilter = req.query.characteristic !== undefined;
+  const queryArgs = [
+    `SELECT 
+      p.id, 
+      p.name, 
+      ARRAY_AGG(c.name) AS characteristics,
+      ${totalScoreCalcQuery} 
+      FROM products p 
+      JOIN products_characteristics pc ON p.id = pc.product_id 
+      JOIN characteristics c ON pc.characteristic_id = c.id 
+      ${hasFilter ? "WHERE c.name = $1" : ""}
+      GROUP BY p.id`,
+    hasFilter ? [req.query.characteristic] : [],
+  ];
   const result = await db.query(...queryArgs);
 
   res.json(result.rows);
@@ -20,12 +26,14 @@ const getFilteredProducts = async (req, res) => {
 const getProductScores = async (_req, res) => {
   // TO DO: Error handling
   const results = await db.query(
-    `SELECT p.name AS product_name,
-    COALESCE(SUM(c.score_value), 0) AS total_score
+    `SELECT 
+    p.id,
+    p.name AS product_name,
+   ${totalScoreCalcQuery}
     FROM products p
     LEFT JOIN products_characteristics pc ON p.id = pc.product_id
     LEFT JOIN characteristics c ON pc.characteristic_id = c.id
-    GROUP BY p.name
+    GROUP BY p.id
     ORDER BY total_score DESC
     `
   );
