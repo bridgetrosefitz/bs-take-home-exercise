4e0b86b6205021c86fd5926684f9bdffacefcc4c
bridgetrosefitz <bridgetrosefitz@gmail.com>
2025-02-04 21:59:32 +1100

    handle no query param for filter

--- api/controllers/products.js
+++ api/controllers/products.js
@@ -2,13 +2,17 @@ import * as db from "../db/index.js";
 
 const getFilteredProducts = async (req, res) => {
   // TO DO: Error handling
-  const result = await db.query(
-    `SELECT p.* FROM products p 
+  const queryArgs =
+    req.query.characteristic === undefined
+      ? [`SELECT * FROM products`]
+      : [
+          `SELECT p.* FROM products p 
     JOIN products_characteristics pc ON p.id = pc.product_id 
     JOIN characteristics c ON pc.characteristic_id = c.id 
     WHERE c.name = $1`,
-    [req.query.characteristic]
-  );
+          [req.query.characteristic],
+        ];
+  const result = await db.query(...queryArgs);
 
   res.json(result.rows);
 };
